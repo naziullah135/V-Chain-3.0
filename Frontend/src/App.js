054bbfb54d2eb1e644c133bcb3d76ec5d7df75a3
jaedalzaber <jaedalzaber@gmail.com>
2022-10-28 14:54:15 +0600

    home page done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Link } from 'react-router-dom';
+
+import Home from './pages/home/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="main-container">
+        {/* <div className="sidebar-dummy"></div> */}
+        <div className="sidebar" >
+          <Link className="logo" to='/'>
+            <img src={logo} width="140px" alt="" />
+          </Link>
+          <div className="profile">
+            profile
+          </div>
+        </div>
+
+        <main id='container' className="main"  >
+          <Home />
+        </main>
+
+        <div className="links">
+          <div className="nav">
+            <div className='nav--item menu active' ><span>Users</span></div>
+            <div className="nav--item menu" >Vehicles</div>
+            <div className="nav--item menu" >Contracts</div>
+            <div className="nav--item menu" >--------------</div>
+            <div className="nav--item menu" >Brands</div>
+            <div className="nav--item menu" >Models</div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ Frontend/src/pages/home/Home.js
@@ -0,0 +1,24 @@
+import styles from './Home.scss'
+import React, { useEffect } from 'react'
+
+
+const Home = React.forwardRef((props, ref) => {
+
+    return (
+        <div className='grid home' >
+            <div className='grid--item home--item-1'>
+                <span className='title-bold mg-l-50'>a</span>
+            </div>
+            <div className='grid--item home--item-2'>
+                <span className='title-bold mg-l-50'>b</span>
+            </div>
+
+            <div className='grid--item home--item-3'>
+                <span className='title-bold mg-l-50'>c</span>
+            </div>
+
+        </div>
+    );
+})
+
+export default Home;
